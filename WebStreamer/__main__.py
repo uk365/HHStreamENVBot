641ac9b91b27975c0d6b4b905a4e9bfa9051ebdb
Parveen Bhadoo <github-notify@hashhackers.com>
2024-07-15 12:27:57 +0530

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -32,37 +32,43 @@ server = web.AppRunner(web_server())
 
 
 async def start_services():
-    print()
-    print("-------------------- Initializing Telegram Bot --------------------")
-    await StreamBot.start()
-    bot_info = await StreamBot.get_me()
-    StreamBot.username = bot_info.username
-    print("------------------------------ DONE ------------------------------")
-    print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
-    await initialize_clients()
-    print("------------------------------ DONE ------------------------------")
-    if Var.ON_HEROKU:
-        print("------------------ Starting Keep Alive Service ------------------")
+    try:
         print()
-        asyncio.create_task(utils.ping_server())
-    print("--------------------- Initalizing Web Server ---------------------")
-    await server.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
-    await web.TCPSite(server, bind_address, Var.PORT).start()
-    print("------------------------------ DONE ------------------------------")
-    print()
-    print("------------------------- Service Started -------------------------")
-    print("                        bot =>> {}".format(bot_info.first_name))
-    if bot_info.dc_id:
-        print("                        DC ID =>> {}".format(str(bot_info.dc_id)))
-    print("                        server ip =>> {}".format(bind_address, Var.PORT))
-    if Var.ON_HEROKU:
-        print("                        app running on =>> {}".format(Var.FQDN))
-    print("------------------------------------------------------------------")
-    await idle()
+        print("-------------------- Initializing Telegram Bot --------------------")
+        await StreamBot.start()
+        bot_info = await StreamBot.get_me()
+        StreamBot.username = bot_info.username
+        print("------------------------------ DONE ------------------------------")
+        print()
+        print(
+            "---------------------- Initializing Clients ----------------------"
+        )
+        await initialize_clients()
+        print("------------------------------ DONE ------------------------------")
+        if Var.ON_HEROKU:
+            print("------------------ Starting Keep Alive Service ------------------")
+            print()
+            asyncio.create_task(utils.ping_server())
+        print("--------------------- Initalizing Web Server ---------------------")
+        await server.setup()
+        bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
+        await web.TCPSite(server, bind_address, Var.PORT).start()
+        print("------------------------------ DONE ------------------------------")
+        print()
+        print("------------------------- Service Started -------------------------")
+        print("                        bot =>> {}".format(bot_info.first_name))
+        if bot_info.dc_id:
+            print("                        DC ID =>> {}".format(str(bot_info.dc_id)))
+        print("                        server ip =>> {}".format(bind_address, Var.PORT))
+        if Var.ON_HEROKU:
+            print("                        app running on =>> {}".format(Var.FQDN))
+        print("------------------------------------------------------------------")
+        await idle()
+    except Exception as e:
+        logging.error(e.with_traceback(None))
+        return web.Response(
+            text='<html> <head> <title>LinkerX CDN</title> <style> body{ margin:0; padding:0; width:100%; height:100%; color:#b0bec5; display:table; font-weight:100; font-family:Lato } .container{ text-align:center; display:table-cell; vertical-align:middle } .content{ text-align:center; display:inline-block } .message{ font-size:80px; margin-bottom:40px } .submessage{ font-size:40px; margin-bottom:40px } .copyright{ font-size:20px; } a{ text-decoration:none; color:#3498db } </style> </head> <body> <div class="container"> <div class="content"> <div class="message">LinkerX CDN</div> <div class="submessage">Server Crash due to Telegram Flood Wait Error</div> <div class="copyright">Hash Hackers and LiquidX Projects</div> </div> </div> </body> </html>', content_type="text/html"
+        )
 
 async def cleanup():
     await server.cleanup()
