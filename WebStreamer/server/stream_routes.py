a0d0e148eabd5f181839ad1162c408a52b33d477
Parveen Bhadoo <github-notify@hashhackers.com>
2024-07-15 12:17:20 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -29,7 +29,7 @@ routes = web.RouteTableDef()
 @routes.get("/", allow_head=True)
 async def root_route_handler(_):
     return web.Response(
-            text="<html> <head><title>LinkerX</title></head> <body> <center><h1>Running Since "+ utils.get_readable_time(time.time() - StartTime) +"</h1></center> <hr><center>nginx</center> </body> </html>", content_type="text/html"
+            text='<html> <head> <title>LinkerX CDN</title> <style> body{ margin:0; padding:0; width:100%; height:100%; color:#b0bec5; display:table; font-weight:100; font-family:Lato } .container{ text-align:center; display:table-cell; vertical-align:middle } .content{ text-align:center; display:inline-block } .message{ font-size:80px; margin-bottom:40px } .submessage{ font-size:40px; margin-bottom:40px } .copyright{ font-size:20px; } a{ text-decoration:none; color:#3498db } </style> </head> <body> <div class="container"> <div class="content"> <div class="message">LinkerX CDN</div> <div class="submessage">All Systems Operational since '+utils.get_readable_time(time.time() - StartTime)+'</div> <div class="copyright">Hash Hackers and LiquidX Projects</div> </div> </div> </body> </html>'
     )
 
 @routes.get("/{path:.*}", allow_head=True)
