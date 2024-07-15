fb7ff92faab696bdc4484ef6fe2ac1281e28d535
Parveen Bhadoo <github-notify@hashhackers.com>
2024-07-15 12:20:58 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -86,82 +86,88 @@ async def not_found(_):
 class_cache = {}
 
 async def media_streamer(request: web.Request, message_id: int, channel_id):
-    range_header = request.headers.get("Range", 0)
-    
-    index = min(work_loads, key=work_loads.get)
-    faster_client = multi_clients[index]
-    
-    if Var.MULTI_CLIENT:
-        logging.info(f"Client {index} is now serving {request.remote}")
+    try:
+        range_header = request.headers.get("Range", 0)
+        
+        index = min(work_loads, key=work_loads.get)
+        faster_client = multi_clients[index]
+        
+        if Var.MULTI_CLIENT:
+            logging.info(f"Client {index} is now serving {request.remote}")
 
-    if faster_client in class_cache:
-        tg_connect = class_cache[faster_client]
-        logging.debug(f"Using cached ByteStreamer object for client {index}")
-    else:
-        logging.debug(f"Creating new ByteStreamer object for client {index}")
-        tg_connect = utils.ByteStreamer(faster_client)
-        class_cache[faster_client] = tg_connect
-    logging.debug("before calling get_file_properties")
-    file_id = await tg_connect.get_file_properties(message_id, channel_id)
-    logging.debug("after calling get_file_properties")
-
-    file_size = file_id.file_size
-
-    if range_header:
-        from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
-        from_bytes = int(from_bytes)
-        until_bytes = int(until_bytes) if until_bytes else file_size - 1
-    else:
-        from_bytes = request.http_range.start or 0
-        until_bytes = (request.http_range.stop or file_size) - 1
-
-    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
-        return web.Response(
-            status=416,
-            body="416: Range not satisfiable",
-            headers={"Content-Range": f"bytes */{file_size}"},
-        )
-
-    chunk_size = 1024 * 1024
-    until_bytes = min(until_bytes, file_size - 1)
-
-    offset = from_bytes - (from_bytes % chunk_size)
-    first_part_cut = from_bytes - offset
-    last_part_cut = until_bytes % chunk_size + 1
-
-    req_length = until_bytes - from_bytes + 1
-    part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
-    body = tg_connect.yield_file(
-        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
-    )
-    mime_type = file_id.mime_type
-    file_name = file_id.file_name
-    disposition = "attachment"
-
-    if mime_type:
-        if not file_name:
-            try:
-                file_name = f"{secrets.token_hex(2)}.{mime_type.split('/')[1]}"
-            except (IndexError, AttributeError):
-                file_name = f"{secrets.token_hex(2)}.unknown"
-    else:
-        if file_name:
-            mime_type = mimetypes.guess_type(file_id.file_name)
+        if faster_client in class_cache:
+            tg_connect = class_cache[faster_client]
+            logging.debug(f"Using cached ByteStreamer object for client {index}")
         else:
-            mime_type = "application/octet-stream"
-            file_name = f"{secrets.token_hex(2)}.unknown"
+            logging.debug(f"Creating new ByteStreamer object for client {index}")
+            tg_connect = utils.ByteStreamer(faster_client)
+            class_cache[faster_client] = tg_connect
+        logging.debug("before calling get_file_properties")
+        file_id = await tg_connect.get_file_properties(message_id, channel_id)
+        logging.debug("after calling get_file_properties")
 
-    if "video/" in mime_type or "audio/" in mime_type or "/html" in mime_type:
-        disposition = "inline"
+        file_size = file_id.file_size
 
-    return web.Response(
-        status=206 if range_header else 200,
-        body=body,
-        headers={
-            "Content-Type": f"{mime_type}",
-            "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Length": str(req_length),
-            "Content-Disposition": f'{disposition}; filename="{file_name}"',
-            "Accept-Ranges": "bytes",
-        },
-    )
+        if range_header:
+            from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
+            from_bytes = int(from_bytes)
+            until_bytes = int(until_bytes) if until_bytes else file_size - 1
+        else:
+            from_bytes = request.http_range.start or 0
+            until_bytes = (request.http_range.stop or file_size) - 1
+
+        if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
+            return web.Response(
+                status=416,
+                body="416: Range not satisfiable",
+                headers={"Content-Range": f"bytes */{file_size}"},
+            )
+
+        chunk_size = 1024 * 1024
+        until_bytes = min(until_bytes, file_size - 1)
+
+        offset = from_bytes - (from_bytes % chunk_size)
+        first_part_cut = from_bytes - offset
+        last_part_cut = until_bytes % chunk_size + 1
+
+        req_length = until_bytes - from_bytes + 1
+        part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
+        body = tg_connect.yield_file(
+            file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
+        )
+        mime_type = file_id.mime_type
+        file_name = file_id.file_name
+        disposition = "attachment"
+
+        if mime_type:
+            if not file_name:
+                try:
+                    file_name = f"{secrets.token_hex(2)}.{mime_type.split('/')[1]}"
+                except (IndexError, AttributeError):
+                    file_name = f"{secrets.token_hex(2)}.unknown"
+        else:
+            if file_name:
+                mime_type = mimetypes.guess_type(file_id.file_name)
+            else:
+                mime_type = "application/octet-stream"
+                file_name = f"{secrets.token_hex(2)}.unknown"
+
+        if "video/" in mime_type or "audio/" in mime_type or "/html" in mime_type:
+            disposition = "inline"
+
+        return web.Response(
+            status=206 if range_header else 200,
+            body=body,
+            headers={
+                "Content-Type": f"{mime_type}",
+                "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
+                "Content-Length": str(req_length),
+                "Content-Disposition": f'{disposition}; filename="{file_name}"',
+                "Accept-Ranges": "bytes",
+            },
+        )
+    except Exception as e:
+        logging.error(f"Error in media streamer: {str(e)}")
+        return web.Response(
+            text='<html> <head> <title>LinkerX CDN</title> <style> body{ margin:0; padding:0; width:100%; height:100%; color:#b0bec5; display:table; font-weight:100; font-family:Lato } .container{ text-align:center; display:table-cell; vertical-align:middle } .content{ text-align:center; display:inline-block } .message{ font-size:80px; margin-bottom:40px } .submessage{ font-size:40px; margin-bottom:40px } .copyright{ font-size:20px; } a{ text-decoration:none; color:#3498db } </style> </head> <body> <div class="container"> <div class="content"> <div class="message">LinkerX CDN</div> <div class="submessage">'+str(e)+'</div> <div class="copyright">Hash Hackers and LiquidX Projects</div> </div> </div> </body> </html>'
+        )
